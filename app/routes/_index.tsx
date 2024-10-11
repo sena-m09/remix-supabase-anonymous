0f81c97429f3a8d4dc800c0f0041eebe8fe7474d
sena-m09 <sena-murakami@gaji.jp>
2024-10-11 15:38:27 +0900

    first commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,19 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <main className="p-6">
+      <h1 className="mb-1">TOP PAGE</h1>
+      <p>texttexttext</p>
+    </main>
+  );
+}
+
+
